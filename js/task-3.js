0d13baf64839988cdf29edef8a247628b72d4151
Maksym Khlysta <maksimhlista1337@gmail.com>
2025-10-29 23:52:12 +0200

    hw4 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,24 +1,18 @@
-function filterArray(numbers, value) {
-    // задаєм новий пустив масив
-    let newArray = [];
-    // numberElement змінна до якої при кожній ітерації буде записуватись  поточний ел масиву/ numbers  зміна яку перебирають по одному знач
-    for (const numberElement of numbers) {
-// якщо ел змінної більший за значення 
-        if (numberElement > value) {
-            // то додаєм до пустого масиву нове знач
-        newArray.push(numberElement);
-        }
-        // якщо ні то з пустим масивом нічого не робим 
-         else {
-        newArray;
-        }
-        
-    }
-//    повертаєм в консоль
-    return newArray;
-}
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    changeUsername (newName) {
+        this.username = newName;
+    },
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    },
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
+};
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
